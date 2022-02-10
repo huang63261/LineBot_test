f8c233d85c62e05f7fe03a575bd58746206343a9
Harvey_Huang <huang63261@gmail.com>
2022-02-10 15:00:28 +0800

    Taipei101ImageMessage

--- app.py
+++ app.py
@@ -50,8 +50,9 @@ def index():
                     payload["messages"] = [getPlayStickerMessage()]
                 elif text == "台北101":
                     payload["messages"] = [getTaipei101ImageMessage(),
-                                           getTaipei101LocationMessage(),
-                                           getMRTVideoMessage()]
+                                        #    getTaipei101LocationMessage(),
+                                        #    getMRTVideoMessage()
+                                           ]
                 elif text == "quoda":
                     payload["messages"] = [
                             {
